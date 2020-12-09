863c653481e0783196ce10a27d348e6a6b3a143c
Mateusz@MSI <matkac98@gmail.com>
2020-12-09 21:52:33 +0100

    Dodanie prawidłowej liczby
    Pokazanie dla użytkownika liczby wylosowanej przez komputer

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ print ("You have to choose the number that probram choose from 1 to 3. Good luck
 los=random.randint(1,3)
 value=input('Wpisz swój typ liczby: ')
 if(los==value):
-    return print("Gratulacje, udało Ci się wygrać !")
+    print("Gratulacje, udało Ci się wygrać !")
 else:
-    return print ("Niestety, przegrałeś ! :(")
+    print ("Niestety, przegrałeś ! :(")
+    print("It was %s"%los)
