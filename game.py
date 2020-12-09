6018f92b7f7066db152902e17f427562347c6752
Mateusz@MSI <matkac98@gmail.com>
2020-12-09 21:45:05 +0100

    Opcja if
    Stworzenie podstawowej funkcji if

--- game.py
+++ game.py
@@ -4,3 +4,7 @@ print("Welcome in game %s"%name)
 print ("You have to choose the number that probram choose from 1 to 3. Good luck !")
 los=random.randint(1,3)
 value=input('Wpisz swój typ liczby: ')
+if(los==value):
+    return print("Gratulacje, udało Ci się wygrać !")
+else:
+    return print ("Niestety, przegrałeś ! :(")
