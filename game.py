575ff0f61a226d5246b645603fc1d15e3843d306
Mateusz@MSI <matkac98@gmail.com>
2020-12-09 21:42:14 +0100

    Los
    Dodanie funkcji losowej do programu

--- game.py
+++ game.py
@@ -1,2 +1,6 @@
+import random
 name=input("Enter your name: ")
 print("Welcome in game %s"%name)
+print ("You have to choose the number that probram choose from 1 to 3. Good luck !")
+los=random.randint(1,3)
+value=input('Wpisz swój typ liczby: ')
